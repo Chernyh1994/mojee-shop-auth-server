2b3eb72fcd706b70ec1951cb4a32c03decea64c5
Nikolay Chernykh <chernuh.1994@gmail.com>
2022-08-04 14:04:45 +0300

    Completed adding unit tests for AuthService.

--- test/units/services/auth.test.ts
+++ test/units/services/auth.test.ts
@@ -13,6 +13,8 @@ import { UserEntity } from '../../../src/entity/user.entity';
 import { RegistrationRequest } from '../../../src/http/requests/auth/registration.request';
 import { LoginRequest } from '../../../src/http/requests/auth/login.request';
 import UnauthorizedException from '../../../src/commons/exceptions/http/unauthorized.exception';
+import { PasswordResetRequest } from '../../../src/http/requests/auth/password-reset.request';
+import { ForgotPasswordRequest } from '../../../src/http/requests/auth/forgot-password.request';
 
 describe('AuthService', () => {
   let authService: AuthService;
@@ -143,13 +145,14 @@ describe('AuthService', () => {
         refreshToken:
           '312baf653be5e165db266a0d421a1bbbf5bb35617b1b72b9bfb911c8046370a53e6b6238cf0728e1774c40b0bf701e49',
       };
-      const validateUser = jest.spyOn(AuthService.prototype as any, 'validateUser');
+      const validateUserMock = jest.spyOn(AuthService.prototype as any, 'validateUser');
 
-      validateUser.mockImplementation(() => user);
+      validateUserMock.mockImplementation(() => user);
       tokenServiceMock.generateTokens.mockResolvedValue(tokens);
       const result = await authService.login(loginRequestMock);
 
-      expect(validateUser).toHaveBeenCalled();
+      expect(validateUserMock).toBeCalledWith(loginRequestMock.email, loginRequestMock.password);
+      expect(validateUserMock).toBeCalledTimes(1);
       expect(tokenServiceMock.generateTokens).toBeCalledWith(user.id);
       expect(tokenServiceMock.generateTokens).toBeCalledTimes(1);
       expect(result).toBe(tokens);
@@ -196,6 +199,174 @@ describe('AuthService', () => {
       expect(resMock.status).toBeCalledWith(HttpStatusCode.FORBIDDEN);
       expect(resMock.status).toBeCalledTimes(1);
       expect(resMock.json).toBeCalledWith({ error: 'Error logout.', status: 403 });
+      expect(resMock.json).toBeCalledTimes(1);
+      expect(nextMock).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('verifyUser', () => {
+    const linkMock = jest.fn() as unknown as string;
+    const user: UserEntity = {
+      id: 2,
+      email: 'test@test.com',
+      password: '$2b$10$kBSeGVlnHjdtUlhhrxsX3.4huPqCW/xNBsLNKlclqx6fEtEuRyOG2',
+      is_verified: false,
+      is_deleted: false,
+      profile_id: null,
+      role_id: 2,
+      created_at: new Date(),
+      updated_at: new Date(),
+    };
+
+    test('Should return message: User has been verified;', async () => {
+      const responseMessage: ResponseMessageType = { data: 'User has been verified.' };
+      const decryptTemporaryLinkMock = jest.spyOn(AuthService.prototype as any, 'decryptTemporaryLink');
+      decryptTemporaryLinkMock.mockImplementation(() => user.id);
+      userServiceMock.verifyUser.mockResolvedValue(user);
+      const result = await authService.verifyUser(linkMock);
+
+      expect(decryptTemporaryLinkMock).toBeCalledWith(linkMock);
+      expect(decryptTemporaryLinkMock).toBeCalledTimes(1);
+      expect(userServiceMock.verifyUser).toBeCalledWith(user.id);
+      expect(userServiceMock.verifyUser).toBeCalledTimes(1);
+      expect(result).toStrictEqual(responseMessage);
+    });
+
+    test('Should return a Forbidden Exception for the verify user method;', async () => {
+      const forbiddenException: HttpException = new ForbiddenException('User not verified.');
+      const decryptTemporaryLinkMock = jest.spyOn(AuthService.prototype as any, 'decryptTemporaryLink');
+      decryptTemporaryLinkMock.mockImplementation(() => user.id);
+      userServiceMock.verifyUser.mockResolvedValue(null);
+      errorHandlerMiddleware(forbiddenException, reqMock, resMock, nextMock);
+
+      await expect(authService.verifyUser(linkMock)).rejects.toThrow(Error);
+      expect(resMock.status).toBeCalledWith(HttpStatusCode.FORBIDDEN);
+      expect(resMock.status).toBeCalledTimes(1);
+      expect(resMock.json).toBeCalledWith({ error: 'User not verified.', status: 403 });
+      expect(resMock.json).toBeCalledTimes(1);
+      expect(nextMock).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('refresh', () => {
+    const refreshTokenMock = jest.fn() as unknown as string;
+
+    test('Should return access token and refresh token;', async () => {
+      const tokens: ResponseTokensType = {
+        accessToken:
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjY3LCJpYXQiOjE2NTk1MjY3MTIsImV4cCI6MTY1OTUzMDMxMn0.TWzhQiVmOVboTpKpT_ZXykG7Ksqh7WNYa-36VpUpHwI',
+        refreshToken:
+          '312baf653be5e165db266a0d421a1bbbf5bb35617b1b72b9bfb911c8046370a53e6b6238cf0728e1774c40b0bf701e49',
+      };
+
+      tokenServiceMock.refreshTokens.mockResolvedValue(tokens);
+      const result = await authService.refresh(refreshTokenMock);
+
+      expect(tokenServiceMock.refreshTokens).toBeCalledWith(refreshTokenMock);
+      expect(tokenServiceMock.refreshTokens).toBeCalledTimes(1);
+      expect(result).toStrictEqual(tokens);
+    });
+
+    test('Should return a Unauthorized Exception for the refresh method;', async () => {
+      const unauthorizedException: HttpException = new UnauthorizedException('Unauthorized.');
+      errorHandlerMiddleware(unauthorizedException, reqMock, resMock, nextMock);
+
+      await expect(authService.refresh(null)).rejects.toThrow(Error);
+      expect(resMock.status).toBeCalledWith(HttpStatusCode.UNAUTHORIZED);
+      expect(resMock.status).toBeCalledTimes(1);
+      expect(resMock.json).toBeCalledWith({ error: 'Unauthorized.', status: 401 });
+      expect(resMock.json).toBeCalledTimes(1);
+      expect(nextMock).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('forgotPassword', () => {
+    const forgotPasswordRequestMock: ForgotPasswordRequest = { email: jest.fn() } as unknown as ForgotPasswordRequest;
+    const user: UserEntity = {
+      id: 2,
+      email: 'test@test.com',
+      password: '$2b$10$kBSeGVlnHjdtUlhhrxsX3.4huPqCW/xNBsLNKlclqx6fEtEuRyOG2',
+      is_verified: false,
+      is_deleted: false,
+      profile_id: null,
+      role_id: 2,
+      created_at: new Date(),
+      updated_at: new Date(),
+    };
+
+    test('Should return message: Email for reset password send;', async () => {
+      const responseMessageMock: ResponseMessageType = { data: 'Email for reset password send.' };
+      userServiceMock.findUserByEmail.mockResolvedValue(user);
+      const encryptTemporaryLinkMock = jest.spyOn(AuthService.prototype as any, 'encryptTemporaryLink');
+      encryptTemporaryLinkMock.mockImplementation(() => user.id);
+      mailServiceMock.sendResetPassword.mockResolvedValue();
+      const result = await authService.forgotPassword(forgotPasswordRequestMock);
+
+      expect(userServiceMock.findUserByEmail).toBeCalledWith(forgotPasswordRequestMock.email);
+      expect(userServiceMock.findUserByEmail).toBeCalledTimes(1);
+      expect(encryptTemporaryLinkMock).toBeCalledWith(user.id);
+      expect(encryptTemporaryLinkMock).toBeCalledTimes(1);
+      expect(mailServiceMock.sendResetPassword).toBeCalledWith(user.email, 'undefined/auth/password-reset/2');
+      expect(mailServiceMock.sendResetPassword).toBeCalledTimes(1);
+      expect(result).toStrictEqual(responseMessageMock);
+    });
+
+    test('Should return a Forbidden Exception for the forgotPassword method;', async () => {
+      const forbiddenException: HttpException = new ForbiddenException('User not found.');
+      userServiceMock.findUserByEmail.mockResolvedValue(null);
+      errorHandlerMiddleware(forbiddenException, reqMock, resMock, nextMock);
+
+      await expect(authService.forgotPassword(forgotPasswordRequestMock)).rejects.toThrow(Error);
+      expect(resMock.status).toBeCalledWith(HttpStatusCode.FORBIDDEN);
+      expect(resMock.status).toBeCalledTimes(1);
+      expect(resMock.json).toBeCalledWith({ error: 'User not found.', status: 403 });
+      expect(resMock.json).toBeCalledTimes(1);
+      expect(nextMock).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('passwordReset', () => {
+    const passwordResetRequestMock: PasswordResetRequest = {
+      password: jest.fn(),
+    } as unknown as PasswordResetRequest;
+    const linkMock = jest.fn() as unknown as string;
+    const user: UserEntity = {
+      id: 2,
+      email: 'test@test.com',
+      password: '$2b$10$kBSeGVlnHjdtUlhhrxsX3.4huPqCW/xNBsLNKlclqx6fEtEuRyOG2',
+      is_verified: false,
+      is_deleted: false,
+      profile_id: null,
+      role_id: 2,
+      created_at: new Date(),
+      updated_at: new Date(),
+    };
+
+    test('Should return message: Password successes changed;', async () => {
+      const responseMessage: ResponseMessageType = { data: 'Password successes changed.' };
+      const decryptTemporaryLinkMock = jest.spyOn(AuthService.prototype as any, 'decryptTemporaryLink');
+      decryptTemporaryLinkMock.mockImplementation(() => user.id);
+      userServiceMock.changePassword.mockResolvedValue(user);
+      const result = await authService.passwordReset(linkMock, passwordResetRequestMock);
+
+      expect(decryptTemporaryLinkMock).toBeCalledWith(linkMock);
+      expect(decryptTemporaryLinkMock).toBeCalledTimes(1);
+      expect(userServiceMock.changePassword).toBeCalledWith(user.id, passwordResetRequestMock.password);
+      expect(userServiceMock.changePassword).toBeCalledTimes(1);
+      expect(result).toStrictEqual(responseMessage);
+    });
+
+    test('Should return a Forbidden Exception for the reset password method;', async () => {
+      const forbiddenException: HttpException = new ForbiddenException('Error change password.');
+      const decryptTemporaryLinkMock = jest.spyOn(AuthService.prototype as any, 'decryptTemporaryLink');
+      decryptTemporaryLinkMock.mockImplementation(() => user.id);
+      userServiceMock.changePassword.mockResolvedValue(null);
+      errorHandlerMiddleware(forbiddenException, reqMock, resMock, nextMock);
+
+      await expect(authService.passwordReset(linkMock, passwordResetRequestMock)).rejects.toThrow(Error);
+      expect(resMock.status).toBeCalledWith(HttpStatusCode.FORBIDDEN);
+      expect(resMock.status).toBeCalledTimes(1);
+      expect(resMock.json).toBeCalledWith({ error: 'Error change password.', status: 403 });
       expect(resMock.json).toBeCalledTimes(1);
       expect(nextMock).not.toHaveBeenCalled();
     });
